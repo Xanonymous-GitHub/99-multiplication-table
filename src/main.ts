9dbbb9dd85a939e7cbf3db4449185d6e5a8244a5
TU-LIN <tu.ty.lin@linecorp.com>
2025-01-31 19:40:17 +0800

    feat: 🎸 add custom font

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ document.querySelector<HTMLDivElement>("#app")!.insertAdjacentHTML(
   `
     <div>
       <img src="${logo}" class="logo" alt="Vite logo" />
-      <h1>Multiplication Table</h1>
+      <h1 class="_42dot-sans">Multiplication Table</h1>
       <div class="card">
-        <code class="read-the-docs">
+        <code class="content-text _42dot-sans">
           <pre>${generateMultiplicationTable(9)}</pre>
         </code>
       </div>
